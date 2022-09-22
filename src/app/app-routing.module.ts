819229f0eb325d91c86d5ad8de65aae4b593493d
h-daria <dariahalaida@gmail.com>
2022-09-22 20:41:51 +0300

    add user page and sign up components, add these components routs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
 import { AuthorizationComponent } from './authorization/authorization.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { UserPageComponent } from './user-page/user-page.component';
 
 const routes: Routes = [
   {path: '', component: MainPageComponent },
-  {path: 'auth', component: AuthorizationComponent}
+  {path: 'auth', component: AuthorizationComponent},
+  {path: 'user-page', component: UserPageComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({

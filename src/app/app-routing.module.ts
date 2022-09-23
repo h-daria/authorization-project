69ca412f019305bdbc771da47bda50a4ec276e08
h-daria <dariahalaida@gmail.com>
2022-09-23 18:44:42 +0300

    fix routs

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from './auth.guard';
 import { AuthorizationComponent } from './authorization/authorization.component';
 import { MainPageComponent } from './main-page/main-page.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 import { UserPageComponent } from './user-page/user-page.component';
 
 const routes: Routes = [
   {path: '', component: MainPageComponent },
   {path: 'auth', component: AuthorizationComponent},
-  {path: 'user-page', component: UserPageComponent, canActivate: [AuthGuard]}
+  {path: 'user-page', component: UserPageComponent, canActivate: [AuthGuard]},
+  {path: 'sign-up-page', component: SignUpComponent}
 ];
 
 @NgModule({

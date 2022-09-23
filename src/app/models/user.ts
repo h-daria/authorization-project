655b68044c06b5a206b5d2cf39ea84e367d05343
h-daria <dariahalaida@gmail.com>
2022-09-23 18:43:10 +0300

    add content to user page

--- src/app/models/user.ts
+++ src/app/models/user.ts
@@ -1,13 +1,9 @@
 export class User {
-    id: string;
     email: string;
     displayName: string;
-    emailVerified: boolean;
 
-    constructor(id: string, email: string, displayName: string, emailVerified: boolean) {
-        this.id = id;
+    constructor( email: string, displayName: string) {
         this.email = email;
         this.displayName = displayName;
-        this.emailVerified = emailVerified;
     }
 }

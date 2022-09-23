a6b666b64fabd845c9cb5f3d9df1fd0b7dc9ac6a
h-daria <dariahalaida@gmail.com>
2022-09-23 19:01:54 +0300

    fix sign up component ts

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -16,32 +16,33 @@ export class SignUpComponent implements OnInit {
 
   ngOnInit(): void {
     this.signUpForm = new FormGroup({
-      'user-email': new FormControl(null, [
-                                            Validators.required, 
-                                            Validators.email, 
-                                            Validators.maxLength(30), 
-                                            Validators.minLength(11)
-                                          ]),
-      'user-name': new FormControl(null, [Validators.required, 
-                                          Validators.pattern('^[a-zA-Z0-9_-]{5,15}$')]),                                    
-      'user-password': new FormControl(null, [Validators.required, Validators.pattern('^[a-zA-Z0-9!_-]{8,20}$'), this.passwordStrength]), 
-      'user-confirm-password': new FormControl(null, [Validators.required, 
-                                                      Validators.pattern('^[a-zA-Z0-9!_-]{8,20}$')]),                                   
-    },
-    { validators: this.validatorsService.passwordMatch('user-password', 'user-confirm-password') }
+        'user-email': new FormControl(null, [
+                                              Validators.required, 
+                                              Validators.email, 
+                                              Validators.maxLength(30), 
+                                              Validators.minLength(11)
+                                            ]),
+        'user-name': new FormControl(null, [Validators.required, 
+                                            Validators.pattern('^[a-zA-Z0-9_-]{5,15}$')]),                                    
+        'user-password': new FormControl(null, [Validators.required, Validators.pattern('^[a-zA-Z0-9!_-]{8,20}$'), this.passwordStrength]), 
+        'user-confirm-password': new FormControl(null, [Validators.required, 
+                                                        Validators.pattern('^[a-zA-Z0-9!_-]{8,20}$')]),                                   
+      },
+      { validators: this.validatorsService.passwordMatch('user-password', 'user-confirm-password') }
     )
   }
 
   passwordStrength(control: FormControl) {
-    const hasUpperCase = /[A-Z]+/.test(control.value);
+      const hasUpperCase = /[A-Z]+/.test(control.value);
 
-        const hasLowerCase = /[a-z]+/.test(control.value);
+      const hasLowerCase = /[a-z]+/.test(control.value);
 
-        const hasNumeric = /[0-9]+/.test(control.value);
+      const hasNumeric = /[0-9]+/.test(control.value);
 
-        const passwordValid = hasUpperCase && hasLowerCase && hasNumeric;
-    if(!passwordValid) {
-      return {passwordNotStrength: true }
+      const passwordValid = hasUpperCase && hasLowerCase && hasNumeric;
+
+      if(!passwordValid) {
+        return { passwordNotStrength: true }
     }
     return null;
   }
